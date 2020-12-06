f895af44b3805f5784831523a74494193ebd26df
alestor123 <alestoraldous@gmail.com>
2020-12-06 20:05:17 +0530

    working on table

---
+++ public/public/scripts/App.js
@@ -0,0 +1,11 @@
+var addButton = document.getElementById('addButton'),
+varAdd = document.getElementById('varAdd'),
+addVarin = document.getElementById('addVarin');
+function Save(){
+if(addVarin.value==""){
+    alert('please fill out input')
+}
+else if(addVarinn.value=="") {
+    alert('please fill out value')
+}
+}
