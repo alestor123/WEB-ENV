2e239535346070d8135d17abf5a75c0dfad67049
alestor123 <alestoraldous@gmail.com>
2020-12-06 17:49:03 +0530

    added home route

--- App.js
+++ App.js
@@ -8,8 +8,14 @@ db = new JSONdb('./store.json'),
 chalk = require('chalk'),
 argv = process.argv[2],
 port = process.env.PORT || argv || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.json({'info':'This is the web-env'})
+})
 app.post('/api/v1', (req, res) => {
     db.set(req.body.key, req.body.value);
-    res.send('Sucess')
+    console.log('Created : '+req.body.key + ' : ' + req.body.value )
+    res.send('Created : '+req.body.key + ' : ' + req.body.value )
 })
 app.listen(port, () => console.log(chalk.green(`Server running at ${port}`)))
--- run.js
+++
@@ -1,7 +0,0 @@
-var axios = require('axios')
-axios.post('http://localhost:3000/api/v1', {
-key:'dddd',
-value:'val'
-}).then((response) => {
-    console.log(response.data);
-})
